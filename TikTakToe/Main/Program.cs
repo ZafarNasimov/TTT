9220551c61a8ab48ebc4bc1b90b04ef310677aa1
Zafar Nasimov <zafarnasimov0662@gmail.com>
2023-05-31 22:07:24 +0500

    Add project files.

---
+++ TikTakToe/Main/Program.cs
@@ -0,0 +1,53 @@
+﻿using TikTakToe.TextConsoleTTT;
+
+namespace TikTakToe.Main
+{
+    public class Program
+    {
+        static int Main()
+        {
+            char[] arr = { '-', '-', '-', '-', '-', '-', '-', '-', '-'};
+            char ch1 = 'x';
+            char ch2 = '0';
+            int flag = 0;
+
+
+            string name1, name2;
+            Console.WriteLine("First player`s name (x): ");
+            name1 = Console.ReadLine();
+            Console.WriteLine("Second player`s name (0): ");
+            name2 = Console.ReadLine();
+
+            while(true)
+            {
+                if (flag == 0)
+                {
+                    if(DuoTextConsoleTTT.PutVal(ch1, name1, arr))
+                        flag++;
+
+                    if (DuoTextConsoleTTT.CheckWinOrDraw(arr) == 1)
+                    {
+                        Console.WriteLine("{0} won ! ", name1);
+                        break;
+                    }
+                }
+                else
+                {
+                    if(DuoTextConsoleTTT.PutVal(ch2, name2, arr))
+                        flag--;
+                    if (DuoTextConsoleTTT.CheckWinOrDraw(arr) == 1)
+                    {
+                        Console.WriteLine("{0} won ! ", name2);
+                        break;
+                    }
+                }
+                if (!new string(arr).Contains('-'))
+                {
+                    Console.WriteLine("Draw ! ");
+                    break;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ TikTakToe/TextConsoleTTT/DuoTextConsoleTTT.cs
@@ -0,0 +1,72 @@
+﻿namespace TikTakToe.TextConsoleTTT
+{
+    public static class DuoTextConsoleTTT
+    {
+        public static bool PutVal(char val,string name,  char[] arr)
+        {
+            Console.WriteLine("    |   |     ");
+            Console.WriteLine("  {0} | {1} | {2} ", arr[0], arr[1], arr[2]);
+            Console.WriteLine("____|___|_____ ");
+            Console.WriteLine("    |   |     ");
+            Console.WriteLine("  {0} | {1} | {2} ", arr[3], arr[4], arr[5]);
+            Console.WriteLine("____|___|_____ ");
+            Console.WriteLine("    |   |     ");
+            Console.WriteLine("  {0} | {1} | {2} ", arr[6], arr[7], arr[8]);
+            Console.WriteLine("    |   | ");
+
+            Console.WriteLine("{0}, enter position to put {1} : ",name ,val);
+            try
+            {
+                int pos = Convert.ToInt32(Console.ReadLine());
+                if (arr[pos] == '-')
+                {
+                    arr[pos] = val;
+                    Console.Clear();
+                } 
+                else
+                {
+                    Console.WriteLine("Position is already occupied. Choose another one !");
+                    return false;
+                }
+                
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static int CheckWinOrDraw(char[] arr)
+        {
+            if (arr[0] == arr[1] && arr[1] == arr[2] && arr[0] != '-')
+                return 1;
+            else if (arr[3] == arr[4] && arr[4] == arr[5] && arr[5] != '-')
+                return 1;
+            else if (arr[6] == arr[7] && arr[7] == arr[8] && arr[8] != '-')
+                return 1;
+            else if (arr[0] == arr[3] && arr[3] == arr[6] && arr[6] != '-')
+                return 1;
+            else if (arr[1] == arr[4] && arr[4] == arr[7] && arr[7] != '-')
+                return 1;
+            else if (arr[2] == arr[5] && arr[5] == arr[8] && arr[8] != '-')
+                return 1;
+            else if (arr[0] == arr[4] && arr[4] == arr[8] && arr[8] != '-')
+                return 1;
+            else if (arr[2] == arr[4] && arr[4] == arr[6] && arr[6] != '-')
+                return 1;
+            else return 0;
+        }
+    }
+}
